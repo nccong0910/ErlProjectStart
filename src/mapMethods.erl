07d2c2a175a90c6d259cc4a00bbdae72f56246de
nccong0910 <nccong0910@gmail.com>
2022-03-07 22:57:24 +0700

    update module mapMethods

--- src/mapMethods.erl
+++ src/mapMethods.erl
@@ -7,7 +7,7 @@
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([size_method/0, filter_method/0]).
+-export([size_method/0, filter_method/0, filtermap_method/0, find_method/0]).
 
 %% ====================================================================
 %% Internal functions
@@ -19,6 +19,15 @@ size_method() ->
 
 filter_method() ->
 	M = #{a => 2, b => 4, c=> 6, "a" => 1, "b" => 2, "c" => 4},
-	Pred = fun (K, V) -> V rem 2 == 0 end,
+	Pred = fun (K, V) -> is_atom(K) andalso V rem 2 == 0 end,
 	maps:filter(Pred, M).
+
+filtermap_method() ->
+	Func = fun(K, V) when is_atom(K) -> {true, V*2}; (_, V) -> V rem 2 == 0 end,
+	M = #{a => 1, b => 2, "c"=> 3, "d" => 4},
+	maps:filtermap(Func, M).
+
+find_method() ->
+	M = #{a => 1, b => 2, c => 3},
+	maps:find(c, M).
 
