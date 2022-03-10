bb2bba65fe39eace5517fb330971132cf949b86a
nccong0910 <nccong0910@gmail.com>
2022-03-10 09:09:51 +0700

    add module reverse

---
+++ src/reverse.erl
@@ -0,0 +1,24 @@
+%% @author attack
+%% @doc @todo Add description to reverse.
+
+
+-module(reverse).
+-export([simple_reverse/1, deep_reverse/1]).
+
+%% simple reverse==================================================================
+simple_reverse(L) -> simple_reverse(L, []).
+
+simple_reverse([], Acc) -> Acc;
+simple_reverse([H | T], Acc) ->
+	simple_reverse(T, [H | Acc]).
+
+%% deep reverse====================================================================
+deep_reverse(L) -> deep_reverse(L, []).
+
+deep_reverse([], Acc) -> Acc;
+deep_reverse([H | T], Acc) ->
+	case H of
+		[_|_] -> deep_reverse(T, [deep_reverse(H) | Acc]);
+		_	  -> deep_reverse(T, [H | Acc])
+	end.
+
